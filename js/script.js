adfc27e3980ced947b30c6ad5a2c8a8a76bcbb32
Dmitriy Vorozheykin <vorozheykin1997@bk.ru>
2021-01-26 16:47:44 +0400

    localStorage

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
         modal = document.querySelector('.modal'),
-        modalTimer = setInterval(openModal, 60000);
+        modalTimer = setInterval(openModal, 9999999);
 
     function closeModal() {
         modal.classList.remove('show');
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', () => {
         modal.classList.remove('hide');
         modal.classList.add('show');
         document.body.style.overflow = 'hidden';
-        // clearInterval(modalTimer);
+        clearInterval(modalTimer);
     }
 
     modalTrigger.forEach((item) => {
@@ -478,6 +478,112 @@ window.addEventListener('DOMContentLoaded', () => {
         });
         
     */
+
+    /**
+     * Calculator
+     */
+
+    let calculatingResult = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+        document.querySelector(`#${sex}`).classList.add('calculating__choose-item_active');
+
+    } else {
+        sex = 'female';
+    }
+
+    if (localStorage.getItem(ratio)) {
+        ratio = localStorage.getItem(ratio);
+    } else {
+        ratio = 1.2;
+    }
+
+    function calculateTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            calculatingResult.textContent = '___';
+            return;
+        }
+
+        if (sex === 'male') {
+            calculatingResult.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        } else {
+            calculatingResult.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        }
+    }
+
+    calculateTotal();
+
+    function getStaticInformation(parentSelector, activityClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', ratio);
+                    // saveToLocalStorage(ratio);
+                } else {
+                    sex = e.target.getAttribute('id');
+                    localStorage.setItem('sex', sex);
+                }
+
+                elements.forEach(elem => {
+                    elem.classList.remove(activityClass);
+                });
+
+                e.target.classList.add(activityClass);
+
+                calculateTotal();
+            });
+        });
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+
+            if (input.value.match(/\D/g)) {
+                input.style.cssText = `box-shadow: 0 4px 20px rgba(161, 17, 17, 0.9)`;
+            } else {
+                input.style.cssText = ``;
+            }
+
+            console.log(typeof (input.value));
+
+            switch (input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    localStorage.setItem('height', height);
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    localStorage.setItem('weight', weight);
+                    break;
+                case 'age':
+                    age = +input.value;
+                    localStorage.setItem('age', age);
+                    break;
+            }
+
+            calculateTotal();
+        });
+
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+
+    // function saveToLocalStorage(source) {
+    //     localStorage.setItem(``, source);
+    // }
+    // Work with localStorage;
 
 
     /** Personal customisation */
