f7f6392c0df9b40d296dcbd91f48987d997296a5
Dmitriy Vorozheykin <lmidorunl@gmail.com>
2021-01-28 21:47:43 +0400

    Full refactoring
    Refactored project for ES6 module standart. Worked on incapsulation

---
+++ js/services/services.js
@@ -0,0 +1,33 @@
+const postData = async (url, data) => {
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: data
+    });
+
+    return await res.json();
+};
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fect ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+};
+
+function getZero(num) {
+    if (num >= 0 && num < 10) {
+        return `0${num}`;
+    } else {
+        return num;
+    }
+}
+
+export { postData };
+export { getResource };
+export { getZero };
