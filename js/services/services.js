6822e115c4285555ecc309ed31b7a13e1344f467
Dmitriy Vorozheykin <lmidorunl@gmail.com>
2021-02-02 18:53:36 +0400

    added polyfils

--- js/services/services.js
+++ js/services/services.js
@@ -14,7 +14,7 @@ const getResource = async (url) => {
     const res = await fetch(url);
 
     if (!res.ok) {
-        throw new Error(`Could not fect ${url}, status: ${res.status}`);
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
     return await res.json();
